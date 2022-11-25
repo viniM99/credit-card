4d78c58e115f56b15a0edd558c7eaa51b5e1c6b3
Vinicius Machado <vinim44@outlook.com>
2022-11-25 20:22:35 -0300

    try fixing js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import IMask from 'imask';
+import imask from 'imask';
 
 const bgColorCircle = document.querySelector(".cc-front .circle-1")
 const bgColorShadow = document.querySelector(".cc-front .circle-2")
@@ -16,33 +16,53 @@ function setCardType(flag) {
 
 globalThis.setCardType = setCardType
 
-// input masks
-const cvcCode = document.getElementById('card-cvc')
+const cvcCode = document.getElementById("card-cvc")
 const cvcCodePattern = {
-    mask: '000'
+    mask: "000"
 }
 
-const cvcCodeMasked = IMask(cvcCode, cvcCodePattern)
+const cvcCodeMasked = imask(cvcCode, cvcCodePattern)
 
 const expirationMonth = document.getElementById('card-month')
 const expirationMonthPattern = {
-    mask: IMask.MaskedRange,
+    mask: imask.MaskedRange,
     from: 1,
     to: 12
 }
 
-const expirationMonthMasked = IMask(expirationMonth, expirationMonthPattern)
+const expirationMonthMasked = imask(expirationMonth, expirationMonthPattern)
 
 const expirationYear = document.getElementById('card-year')
 const expirationYearPattern = {
   mask: "YY",
   blocks: {
     YY: {
-      mask: IMask.MaskedRange,
+      mask: imask.MaskedRange,
       from: String(new Date().getFullYear()).slice(2),
       to: String(new Date().getFullYear() + 10).slice(2),
     },
   },
 }
 
-const expirationYearMasked = IMask(expirationYear, expirationYearPattern)
+const expirationYearMasked = imask(expirationYear, expirationYearPattern)
+
+const cardNumber = document.getElementById('card-number')
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: 'visa',
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, '')
+    const foundMask = dynamicMasked.compiledMasks.find(function(item) {
+      return number.match(item.regex)
+    })
+
+    return foundMask
+  }
+}
+
+const cardNumberMasked = imask(cardNumber, cardNumberPattern)
